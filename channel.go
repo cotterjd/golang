7d3cf31fb51d2d69108c250092ad99db75955f75
Jordan Cotter <jrdn.cttr@gmail.com>
2022-03-25 23:11:57 -0500

    add comments

--- channel.go
+++ channel.go
@@ -6,19 +6,18 @@ import (
 )
 
 var wg = sync.WaitGroup{}
-
+// channel is to data between goroutines
 func main() {
 	ch := make(chan int)
-	wg.Add(2)
+	wg.Add(2) // add wait time (passing 0, for example, won't give time for program to run. Will have empty output)
 	go func() {
 		i := <- ch // take data out of channel
 		fmt.Println(i)
-		wg.Done()
+		wg.Done() // channel will lock and program will panic without ending channel
 	}()
 	go func() {
 		ch <- 42 // put data into channel
-		wg.Done()
+		wg.Done() // channel will lock and program will panic without ending channel
 	}()
-	wg.Wait()
+	wg.Wait() // wait for goroutines
 }
-
